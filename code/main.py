0acdc69445a408fdcee520b4f8744a6eaa3110e9
Tanton-scu <167207223+Tanton-scu@users.noreply.github.com>
2025-07-16 20:46:01 +0800

    Add files via upload

--- code/main.py
+++ code/main.py
@@ -1,14 +1,20 @@
 from util.SaveToCSV import run_with_budget
-from generalTuners import RandomSearch
+from ReadDataset import get_data
+from generalTuners import RandomSearch,BruteForce,SA,HillClimbing,GA,BO,ES,CMAES,DE,PSO,TabuSearch
 from systemTuners import BestConfig
-from algorithmTuners import HABO
+from algorithmTuners import HABO,GGA,ParamILS,SMAC,DEHB,HB
 if __name__ == "__main__":
     kwargs = {
-        "budget": 30,
+        "budget": 200,
         "seed": 1,
-        "filename": "D:/MyProj/PythonProj/CrossDomainTuners/code/algorithmDatasets/cifar10_modified.csv",
+        "filename": "D:/MyProj/PythonProj/CrossDomainTuners/code/algorithmDatasets/CNN_cifar10.csv",
+        #"filename": "D:/MyProj/PythonProj/CrossDomainTuners/code/systemDatasets/7z.csv",
+        #"filename": "D:/MyProj/PythonProj/CrossDomainTuners/code/systemDatasets/PostgreSQL.csv",
     }
     #D:/MyProj/PythonProj/CrossDomainTuners/code/systemDatasets/7z.csv
-    #D:/MyProj/PythonProj/CrossDomainTuners/code/algorithmDatasets/cifar10_modified.csv
-    result = run_with_budget(RandomSearch.run_tuners, **kwargs)
+    #D:/MyProj/PythonProj/CrossDomainTuners/code/algorithmDatasets/CNN_cifar10.csv
+    # 在 main 函数中读取数据
+    file = get_data(kwargs["filename"])
+    kwargs["file"] = file  # 将读取的数据添加到 kwargs 中
+    result = run_with_budget(HB.run_hyperband, **kwargs)
     print(f"调优结果: {result}")
